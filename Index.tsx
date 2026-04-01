6b09197661ae260fd32006609be8a188b3b361bf
mogalmohammadbaig <mogalmohammadbaig98@gmail.com>
2026-04-01 09:26:22 +0530

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import HowItWorksSection from "@/components/HowItWorksSection";
+import StatsSection from "@/components/StatsSection";
+import WhyUsSection from "@/components/WhyUsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <ServicesSection />
+      <StatsSection />
+      <HowItWorksSection />
+      <WhyUsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
